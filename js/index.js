64d1a72dbfff3456cfa6369afdef5d905e364b50
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-17 14:35:44 -0300

    first

--- js/index.js
+++ js/index.js
@@ -1,22 +1,17 @@
-// $.ajax({
-//     url: 'https://explorando.now.sh/posts',
-//     method: 'GET',
-//     dataType: 'json'
-// }).done(function(data) {
-//     arr = data.reverse()
-//     arr.forEach(element => {
-//         $.ajax({
-//             url: 'https://explorando.now.sh/posts/' + element,
-//             method: 'GET',
-//         }).done(function(cont) {
-//             let line1 = cont.split('\n')[0];
-//             let nTitle = line1.replace("#", "").trim()
-//             document.getElementById("screen").innerHTML +=
-//                 `<a class="links" href="view.html?p=${element}">${nTitle}</a>`
-//         });
-//     });
-// });
+arr = []
 
-$.get("./posts", function(response) {
-    console.log(response)
-})
+for (let i = 1; i <= 2; i++) {
+    arr.push(i)
+}
+
+arr.forEach(element => {
+
+    $.get("./posts/" + element + ".md", function(cont) {
+        if (cont) {
+            let line1 = cont.split('\n')[0];
+            let nTitle = line1.replace("#", "").trim()
+            document.getElementById("screen").innerHTML +=
+                `<a class="links" href="view.html?p=${element}.md">${nTitle}</a>`
+        }
+    });
+});
