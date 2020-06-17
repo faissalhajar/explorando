f5ce8e4d0bed612a750021cbd0e030e0430bd314
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-17 14:25:00 -0300

    first

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
-$.ajax({
-    url: 'https://explorando.now.sh/posts',
-    method: 'GET',
-    dataType: 'json'
-}).done(function(data) {
-    arr = data.reverse()
-    arr.forEach(element => {
-        $.ajax({
-            url: 'https://explorando.now.sh/posts/' + element,
-            method: 'GET',
-        }).done(function(cont) {
-            let line1 = cont.split('\n')[0];
-            let nTitle = line1.replace("#", "").trim()
-            document.getElementById("screen").innerHTML +=
-                `<a class="links" href="view.html?p=${element}">${nTitle}</a>`
-        });
-    });
-});
+// $.ajax({
+//     url: 'https://explorando.now.sh/posts',
+//     method: 'GET',
+//     dataType: 'json'
+// }).done(function(data) {
+//     arr = data.reverse()
+//     arr.forEach(element => {
+//         $.ajax({
+//             url: 'https://explorando.now.sh/posts/' + element,
+//             method: 'GET',
+//         }).done(function(cont) {
+//             let line1 = cont.split('\n')[0];
+//             let nTitle = line1.replace("#", "").trim()
+//             document.getElementById("screen").innerHTML +=
+//                 `<a class="links" href="view.html?p=${element}">${nTitle}</a>`
+//         });
+//     });
+// });
+
+$.get("./posts", function(response) {
+    console.log(response)
+})
