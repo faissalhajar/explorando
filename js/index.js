c467cf9ab71ec38ffb2e34f84900c5fc4f99b7b3
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-17 15:07:15 -0300

    first

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 arr = []
 
-for (let i = 1; i <= 2; i++) {
+for (let i = 1; i <= 10; i++) {
     arr.push(i)
 }
 
@@ -8,10 +8,12 @@ arr.forEach(element => {
 
     $.get("./posts/" + element + ".md", function(cont) {
         if (cont) {
-            let line1 = cont.split('\n')[0];
-            let nTitle = line1.replace("#", "").trim()
+            let line2 = cont.split('\n')[1];
+            let spotify = (cont.split('\n')[0])
+            let trimSpot = spotify.substring(5, (spotify.length - 4));
+            let nTitle = line2.replace("#", "").trim()
             document.getElementById("screen").innerHTML +=
-                `<a class="links" href="view.html?p=${element}.md">${nTitle}</a>`
+                `<a class="links" href="view.html?p=${element}.md&s=${trimSpot}">${nTitle}</a>`
         }
     });
 });
--- js/view.js
+++ js/view.js
@@ -3,10 +3,16 @@ function getURLParameter(name) {
 }
 
 let post = getURLParameter('p');
+let spotify = getURLParameter('s');
 
 $.get("./posts/" + post, function(response) {
     let converter = new showdown.Converter({ tables: true, strikethrough: true });
     let text = response
     let html = converter.makeHtml(text);
+    document.getElementById("screen").innerHTML +=
+        `
+    <iframe style="margin: 30px auto 0 auto;" src="https://open.spotify.com/embed-podcast/episode/${spotify}" width="334" height="152" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+    `
     document.getElementById("screen").innerHTML += html
 })
