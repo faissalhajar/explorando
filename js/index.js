6506b64155291bdec11b1cd86800a2f7919b11fa
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-23 09:10:46 -0300

    3

--- js/index.js
+++ js/index.js
@@ -11,8 +11,9 @@ rev.forEach(element => {
     $.get("./posts/" + element + ".md", function(cont) {
         if (cont) {
             let line2 = cont.split('\n')[1];
+            let date = (cont.split('\n')[2]).replace("*", "").replace("*", "")
             let nTitle = line2.replace("#", "").trim()
-            let first2 = `<span class="white">${nTitle.substring(0,5)}</span>` + nTitle.substring(5)
+            let first2 = `<span class="white">${nTitle.substring(0,5)}</span>` + nTitle.substring(5) + ` <br> <div class="date">${date}</div>`
             document.getElementById("screen").innerHTML +=
                 `<a class="links" href="view.html?p=${element}"><div class="linkBox">${first2}</div></a>`
         }
