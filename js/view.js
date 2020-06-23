82b16d1bf5d90a27ca0ecb2f2d8dcbc294852cfa
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-23 09:32:49 -0300

    3

--- js/view.js
+++ js/view.js
@@ -1,10 +1,10 @@
-function getURLParameter(name) {
-    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-}
+// function getURLParameter(name) {
+//     return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+// }
 
-let post = getURLParameter('p');
+let post = (window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)).replace(".html", "")
 
-$.get("./posts/" + post + ".md", function(response) {
+$.get("../posts/" + post + ".md", function(response) {
     let converter = new showdown.Converter({ tables: true, strikethrough: true });
     let text = response
     let html = converter.makeHtml(text);
@@ -13,7 +13,7 @@ $.get("./posts/" + post + ".md", function(response) {
     document.getElementById("screen").innerHTML +=
         `
     <iframe style="margin: 30px auto 0 auto;" src="https://open.spotify.com/embed-podcast/episode/${trimSpot}" width="334" height="152" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-
+    
     `
     document.getElementById("screen").innerHTML += html
 })
