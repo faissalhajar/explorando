6f4475bb7a46f69ce2e4b88f4729e6db97fbd68e
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-06-23 01:03:53 -0300

    3

--- js/index.js
+++ js/index.js
@@ -12,8 +12,9 @@ rev.forEach(element => {
         if (cont) {
             let line2 = cont.split('\n')[1];
             let nTitle = line2.replace("#", "").trim()
+            let first2 = `<span class="white">${nTitle.substring(0,5)}</span>` + nTitle.substring(5)
             document.getElementById("screen").innerHTML +=
-                `<a class="links" href="view.html?p=${element}"><div class="linkBox">${nTitle}</div></a>`
+                `<a class="links" href="view.html?p=${element}"><div class="linkBox">${first2}</div></a>`
         }
     });
 });
