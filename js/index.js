8c09b27a57edbd26ddc8995f35c95e3319efa1a1
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-10-31 18:00:08 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,21 +1,24 @@
 arr = []
 
-for (let i = 1; i <= 3; i++) {
+for (let i = 1; i <= 2; i++) {
     arr.push(i)
 }
 
 rev = arr.reverse()
-
+let p = 1
 rev.forEach(element => {
-
     $.get("./posts/" + element + ".md", function(cont) {
         if (cont) {
             let line2 = cont.split('\n')[1];
             let date = (cont.split('\n')[2]).replace("*", "").replace("*", "")
             let nTitle = line2.replace("#", "").trim()
-            let first2 = `<span class="white">${nTitle.substring(0,5)}</span>` + nTitle.substring(5) + ` <br> <div class="date">${date}</div>`
+            let first2 = nTitle + ` <br> <div class="date">${date}</div>`
             document.getElementById("screen").innerHTML +=
-                `<a class="links" href="./p/${element}.html"><div class="linkBox">${first2}</div></a>`
+                `<a class="links" href="./p/${element}.html"><div class="linkBox">
+                <img class="cov" src="./capas/${p}-capa.png"><br>
+                ${first2}
+                </div></a>`
         }
+        p++
     });
 });
--- js/view.js
+++ js/view.js
@@ -7,13 +7,26 @@ let post = (window.location.pathname.substring(window.location.pathname.lastInde
 $.get("../posts/" + post + ".md", function(response) {
     let converter = new showdown.Converter({ tables: true, strikethrough: true });
     let text = response
+    let line2 = response.split('\n')[1];
+    let nTitle = line2.replace("#", "").trim()
     let html = converter.makeHtml(text);
     let spotify = (text.split('\n')[0])
     let trimSpot = spotify.substring(5, (spotify.length - 4));
+    document.getElementById("herospot").innerHTML +=
+        `
+        <style>
+        .hero {
+            background-image: url(../capas/${post}-capa.png)
+        }
+        </style>
+<div class="hero enableBlur"></div>
+<div class="herot disableBlur">${nTitle}</div>
+`
     document.getElementById("screen").innerHTML +=
         `
-    <iframe style="margin: 30px auto 0 auto;" src="https://open.spotify.com/embed-podcast/episode/${trimSpot}" width="334" height="152" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    
-    `
+<iframe style="margin: 30px auto 0 auto;" src="https://open.spotify.com/embed-podcast/episode/${trimSpot}" width="234" height="152" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+`
     document.getElementById("screen").innerHTML += html
+
 })
