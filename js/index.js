df4e6f01ae0092272d28304be07d6b02d808a3fd
faissalhajar <faissalhajar.ufpr@gmail.com>
2020-11-01 23:56:16 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 arr = []
 
-for (let i = 1; i <= 2; i++) {
+for (let i = 1; i <= 1; i++) {
     arr.push(i)
 }
 
 rev = arr.reverse()
-let p = 1
 rev.forEach(element => {
     $.get("./posts/" + element + ".md", function(cont) {
         if (cont) {
@@ -15,10 +14,9 @@ rev.forEach(element => {
             let first2 = nTitle + ` <br> <div class="date">${date}</div>`
             document.getElementById("screen").innerHTML +=
                 `<a class="links" href="./p/${element}.html"><div class="linkBox">
-                <img class="cov" src="./capas/${p}-capa.png"><br>
+                <img class="cov" src="./capas/${element}-capa.png"><br>
                 ${first2}
                 </div></a>`
         }
-        p++
     });
 });
